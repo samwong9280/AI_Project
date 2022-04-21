3fef7ea687c25bda79698f972e0623148bd4f131
wong0613 <wong0613@umn.edu>
2022-04-21 17:33:16 -0500

    updated iter3

--- Iter3.py
+++ Iter3.py
@@ -1,4 +1,5 @@
 
+from pkg_resources import ContextualVersionConflict
 import constraint
 from constraint import *
 import random
@@ -14,16 +15,11 @@ class Wordle():
         self.goal=goal
         self.freqs=freqs
         self.greenLtrIndex=[]
+        self.yellowLtrsIndex=[]
         self.greenLtrs=[]
         self.yellowLtrs=[]
         print("Starting with: " + str(len(self.words)) + " possible words")
         self.prob.addVariable("w",self.words)
-        # self.prob.addConstraint(lambda x: x in self.words,"w")
-        # self.prob.addConstraint(lambda x: x[0]=='a',"w")
-        # x=self.prob.getSolutions()
-        # self.prob.addConstraint(lambda x: x[1]=='b',"w")
-        # x=self.prob.getSolutions()
-        #print(x)
 
     #Testing function, allows player to guess, not intended for anything useful at the moment 
     def playGame(self):
@@ -42,111 +38,38 @@ class Wordle():
                 print("Incorrect Guess!")
     
     def getFeedback(self,guess,solution,count):
-        #this iteration should only add black letter constraints for the 1st and 2nd guess, then for the 3rd guess we implement all constraints
-        #this should give us the most optimally culled guess pool
-        #still need a heuristic to pickaf
-        #adding in constraints based on feedback from game
-        #for some reason this needed to be hard coded for now, looping doesn't work too well
-        g1=guess[0]
-        g2=guess[1]
-        g3=guess[2]
-        g4=guess[3]
-        g5=guess[4]
-        s1=solution[0]
-        s2=solution[1]
-        s3=solution[2]
-        s4=solution[3]
-        s5=solution[4]
         self.prob.addConstraint(lambda x: x!=guess,"w")
-        #if statements are "green" feedback, so letter is in the right spot
-        #elif statements are "yellow" feedback, so letter is somewhere in the word
-        #else statements are "black" feedback, so letter is not in the word
-        if count<2:
-            if( g1== s1 ):
-                self.greenLtrs.append(g1)
-                self.greenLtrIndex.append( (g1,0) )
-            elif(g1 in solution):
-                self.yellowLtrs.append(g1)
-            else: 
-                self.prob.addConstraint(lambda x: g1 not in x, "w")
-            if( g2== s2 ):
-                self.greenLtrs.append(g2)
-                self.greenLtrIndex.append( (g2,1) )
-            elif(g2 in solution):
-                self.yellowLtrs.append(g2)
-            else: 
-                self.prob.addConstraint(lambda x: g2 not in x, "w")
-            if( g3== s3 ):
-                self.greenLtrs.append(g3)
-                self.greenLtrIndex.append( (g3,2) )
-            elif(g3 in solution):
-                self.yellowLtrs.append(g3)
-            else: 
-                self.prob.addConstraint(lambda x: g3 not in x, "w")
-            if( g4== s4 ):
-                self.greenLtrs.append(g4)
-                self.greenLtrIndex.append( (g4,3) )
-            elif(g4 in solution):
-                self.yellowLtrs.append(g4)
-            else: 
-                self.prob.addConstraint(lambda x: g4 not in x, "w")
-            if( g5== s5 ):
-                self.greenLtrs.append(g5)
-                self.greenLtrIndex.append( (g5,4) )
-            elif(g5 in solution):
-                self.yellowLtrs.append(g5)
-            else: 
-                self.prob.addConstraint(lambda x: g5 not in x, "w")
-        else:
-            #add previously found green and yellow ltr constraints:
-            if(count==2):
-                print("adding in previously found constraints")
-                for tup in self.greenLtrIndex:
-                    self.prob.addConstraint(lambda x, l=tup: x[l[1]]==l[0],"w")
-                for ltr in self.yellowLtrs:
-                    self.prob.addConstraint(lambda x,l = ltr: l in x, "w")
-                self.prob.addConstraint(lambda x: x in self.solutions,"w")
-                #add in any new found constraints
-            if( g1== s1 ):
-                self.greenLtrs.append(g1)
-                self.prob.addConstraint(lambda x: x[0]==g1, "w")
-            elif(g1 in solution):
-                self.yellowLtrs.append(g1)
-                self.prob.addConstraint(lambda x : g1 in x, "w")
-            else: 
-                self.prob.addConstraint(lambda x: g1 not in x, "w")
-            if( g2== s2 ):
-                self.greenLtrs.append(g2)
-                self.prob.addConstraint(lambda x: x[1]==g2, "w")
-            elif(g2 in solution):
-                self.yellowLtrs.append(g2)
-                self.prob.addConstraint(lambda x : g2 in x, "w")
-            else: 
-                self.prob.addConstraint(lambda x: g2 not in x, "w")
-            if( g3== s3 ):
-                self.greenLtrs.append(g3)
-                self.prob.addConstraint(lambda x: x[2]==g3, "w")
-            elif(g3 in solution):
-                self.yellowLtrs.append(g3)
-                self.prob.addConstraint(lambda x : g3 in x, "w")
-            else: 
-                self.prob.addConstraint(lambda x: g3 not in x, "w")
-            if( g4== s4 ):
-                self.greenLtrs.append(g4)
-                self.prob.addConstraint(lambda x: x[3]==g4, "w")
-            elif(g4 in solution):
-                self.yellowLtrs.append(g4)
-                self.prob.addConstraint(lambda x : g4 in x, "w")
-            else: 
-                self.prob.addConstraint(lambda x: g4 not in x, "w")
-            if( g5== s5 ):
-                self.greenLtrs.append(g5)
-                self.prob.addConstraint(lambda x: x[4]==g5, "w")
-            elif(g5 in solution):
-                self.yellowLtrs.append(g5)
-                self.prob.addConstraint(lambda x : g5 in x, "w")
-            else: 
-                self.prob.addConstraint(lambda x: g5 not in x, "w")
+        self.prob.addConstraint(lambda x: x in self.solutions,"w")
+        if count==2:
+            print("adding in previously found constraints")
+            for tup in self.greenLtrIndex:
+                self.prob.addConstraint(lambda x, l=tup: x[l[1]]==l[0],"w")
+            for tup in self.yellowLtrsIndex:
+                self.prob.addConstraint(lambda x,l =tup[0]: l in x, "w")
+                self.prob.addConstraint(lambda x,l=tup[0],n=tup[1]:x[n]!=l,"w")
+
+        for i in range(len(guess)):
+            #don't add in constraints yet so we have more freedom guessing second word
+            if( count<2):
+                if guess[i]==solution[i]:
+                    self.greenLtrs.append(guess[i])
+                    self.greenLtrIndex.append( (guess[i],i))
+                elif guess[i] in solution:
+                    self.yellowLtrs.append(guess[i])
+                    self.yellowLtrsIndex.append( (guess[i],i))
+                else:
+                    self.prob.addConstraint(lambda x,l=guess[i]: l not in x,"w")
+            #after second guess add in all constraints
+            else:    
+                if guess[i] == solution[i]:
+                    self.prob.addConstraint(lambda x,l=guess[i],n=i: x[n]==l,"w")
+                    self.greenLtrs.append(guess[i])
+                elif guess[i] in solution:
+                    self.prob.addConstraint(lambda x,l=guess[i]: l in x, "w")
+                    self.prob.addConstraint(lambda x,l=guess[i],n=i: x[n]!=l,"w")
+                    self.yellowLtrs.append(guess[i])
+                else:
+                    self.prob.addConstraint(lambda x,l=guess[i]: l not in x,"w")
 
     #read solution output into list
     #this elimates words from the possible list of guesses
@@ -158,21 +81,28 @@ class Wordle():
         return newPool
 
     def makeGuess(self,pool):
-        maxScore=0
+        maxScore=999
         curGuess=""
+        # for word in pool:
+        #     curScore=0
+        #     #check words score against current words score(herusitic) and pick the max of the two
+        #     for ltr in word:
+        #         if ltr not in self.greenLtrs:
+        #             curScore+=1
+        #         if ltr not in self.yellowLtrs:
+        #             curScore+=1
+        #         if self.countLtr(word,ltr)==1:
+        #             curScore+=1
+        #         if ltr in self.vowels:
+        #             curScore+=1
+        #     if curScore>maxScore:
+        #         maxScore=curScore
+        #         curGuess=word
         for word in pool:
-            curScore=0
-            #check words score against current words score(herusitic) and pick the max of the two
-            for ltr in word:
-                if ltr not in self.greenLtrs:
-                    curScore+=1
-                if ltr not in self.yellowLtrs:
-                    curScore+=1
-                if self.countLtr(word,ltr)==1:
-                    curScore+=1
-                if ltr in self.vowels:
-                    curScore+=1
-            if curScore>maxScore:
+            curScore = 0
+            for i in range(len(word)):
+                curScore += self.freqs[word[i]][i]
+            if(curScore<maxScore):
                 maxScore=curScore
                 curGuess=word
         return curGuess
@@ -190,7 +120,7 @@ class Wordle():
         print("Trying to guess: " + solution)
         pool= self.words
         count=0
-        guess="crane"
+        guess="salet"
         while(1):
             count+=1
             if(count!=1):
@@ -289,15 +219,13 @@ def getFreqs(words):
             freqs[key][i]=normalize(freqs[key][i],minF,maxF)
     return freqs
     
-        
 def normalize(x, minX, maxX):
     return (x-minX)/(maxX-minX)
-    
+
 def main():
     solutions=genSolutions()
     words=genWords(solutions)
     freqs=getFreqs(words)
-    print(freqs)
     goal=random.choice(solutions)
     response = input("Run Wordle Algorithm?\nType A if you want to see an average guess # over 100 tries\nType Q to run over all possible solutions (warning takes awhile)\nType Y/N to run once/cancel")
     if(response == "Y" or response =="y"):
@@ -312,6 +240,5 @@ def main():
         print("average # of guesses is: " + str(total/100))
     else:
         return None
-
 if __name__=="__main__":
     main()
